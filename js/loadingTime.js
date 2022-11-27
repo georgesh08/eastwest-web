9c4c539d018f622cb44c1dc5fdba54e721e9be4d
georgesh08 <georgiyshulyak@gmail.com>
2022-11-27 20:07:17 +0300

    add loading time for page

---
+++ js/loadingTime.js
@@ -0,0 +1,7 @@
+var before_loadtime = new Date().getTime();
+window.onload = function () {
+    var after_loadtime = new Date().getTime();
+    pgloadtime = (after_loadtime - before_loadtime) / 1000
+
+    document.getElementById("loadTime").innerHTML = "Page load time is " + pgloadtime + " seconds";
+}
